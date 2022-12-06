0925746aa411637b8aca219ecc33950901774353
Becca Williams <rebeccawilliams9941@gmail.com>
2022-12-06 19:14:26 +1100

    :fire: Don't re-write the seeding wheel.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+if !Rails.env.production?
+  # This code will only run in development and test environments
+  Movie.destroy_all
+
+  Movie.create!([{
+                   title: "Iron Man",
+                   rating: "PG-13",
+                   total_gross: 585366247,
+                   description: "Tony Stark builds an armoured suit to fight the throes of evil.",
+                   released_on: "2008-05-02"
+                 },
+                 {
+                   title: "Superman",
+                   rating: "PG",
+                   total_gross: 300451603,
+                   description: "Clark Kent grows up to be the greatest super-hero.",
+                   released_on: "1978-12-15"
+                 },
+                 {
+                   title: "Spider-Man",
+                   rating: "PG-13",
+                   total_gross: 825025036,
+                   description: "Peter Parker gets bitten by a genetically modified spider",
+                   released_on: "2002-05-03"
+                 }])
+
+  p "Created #{Movie.count} Movies"
+end
